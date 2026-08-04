94f095c4202fadc3307f33c0f4767312f4aa5da2
katemzr <katemzr@gmail.com>
2026-08-04 10:10:36 -0500

    Completed Calculator and String Utilities excercise.

--- src/main/java/org/example/StringUtilities.java
+++ src/main/java/org/example/StringUtilities.java
@@ -5,12 +5,24 @@ public class StringUtilities {
 
     // Add a static function called shortString that takes a String parameter and returns true if the string has fewer than 5
     // characters
-
+    public static boolean shortString(String string) {
+        return string.length() < 5;
+    }
     // Add a static function called firstLetter that takes a String parameter and returns the first character of the string
-
+    public static char firstLetter(String string) {
+        return string.charAt(0);
+    }
     // Add a static function called censorAsparagus that takes a String parameter and returns the string with all instances of
     // "asparagus" (lowercase only) replaced with 4 stars: "****"
-
+    public static String censorAsparagus(String string) {
+        return string.replace("asparagus", "****");
+    }
     // Add a static function called bigger that takes two String parameters and returns the longer of the two strings. If the
     // strings are the same length, return the first string.
+    public static String bigger(String string1, String string2) {
+        return (string1.length() >= string2.length()) ? string1 : string2;
+    }
+
 }
+
+
